6e456b38ff7c345e956b2bcfe76dcce7265e0317
jihyeChoi <cfgty12@naver.com>
2017-06-30 14:57:47 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,44 @@
+class HomeController < ApplicationController
+    def index
+        @rsp = (1..3).to_a
+        @select_menu = @rsp
+    end
+    def first
+        @abc = (4..6).to_a
+    end
+    def second
+     @abc = (4..6).to_a
+    end
+    def third
+     @abc = (4..6).to_a
+    end
+    def a
+        @abc = (1..3).to_a
+    end
+    def b
+        @abc = (1..3).to_a
+    end
+    def c
+        @abc = (1..3).to_a
+    end
+    def d
+        @abc = (1..3).to_a
+    end
+    def e
+        @abc = (1..3).to_a
+    end
+    def f
+        
+        @abc = (1..3).to_a
+    end
+    def g
+        @abc = (1..3).to_a
+    end
+    def h
+        @abc = (1..3).to_a
+    end
+    def i
+        @abc = (1..3).to_a
+    end
+    
+end
